7db486c210117494d7fe957f0aed24240c87f9fa
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-04-10 17:25:41 +0900

    Initial Commit

---
+++ NC1-To-Do/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NC1-To-Do
+//
+//  Created by 조세연 on 4/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NC1-To-Do/NC1_To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_To_DoApp.swift
+//  NC1-To-Do
+//
+//  Created by 조세연 on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NC1_To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
